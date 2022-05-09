a741d466960e148a053fd0219004f6a447d2beb4
jie <f5666889092@gmail.com>
2022-05-09 22:03:39 +0800

    step: sort cost

--- module/sortNode.py
+++ module/sortNode.py
@@ -1,7 +1,15 @@
 def sortNode (dataList, cost_or_time):
     for i in range(len(dataList)):
         for j in range(i, len(dataList)):
+            change = 0
             if (dataList[i].data[cost_or_time] < dataList[j].data[cost_or_time]):
+                change = 1
+            elif (dataList[i].data[cost_or_time] == dataList[j].data[cost_or_time]) and cost_or_time== 1:
+                if (dataList[i].data[0] < dataList[j].data[0]):
+                    change = 1
+            if change == 1:
                 temp = dataList[i]
                 dataList[i] = dataList[j]
-                dataList[j] = temp  
+                dataList[j] = temp
+                
+                    
